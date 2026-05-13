c202707b1864f09a86c2682fcbc3d36069f1325c
LeTrungHieu2511 <trunghieule25112005@gmail.com>
2026-05-13 23:13:00 +0700

    Phóng to cửa sổ đăng nhập

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -45,51 +45,54 @@
             // labelUsername
             // 
             this.labelUsername.AutoSize = true;
-            this.labelUsername.Location = new System.Drawing.Point(24, 24);
+            this.labelUsername.Location = new System.Drawing.Point(30, 30);
             this.labelUsername.Name = "labelUsername";
-            this.labelUsername.Size = new System.Drawing.Size(94, 15);
+            this.labelUsername.Size = new System.Drawing.Size(130, 22);
             this.labelUsername.TabIndex = 0;
             this.labelUsername.Text = "Email sinh viên:";
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(24, 44);
+            this.txtUsername.Location = new System.Drawing.Point(30, 60);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(300, 23);
+            this.txtUsername.Size = new System.Drawing.Size(520, 30);
             this.txtUsername.TabIndex = 1;
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             // 
             // labelPassword
             // 
             this.labelPassword.AutoSize = true;
-            this.labelPassword.Location = new System.Drawing.Point(24, 80);
+            this.labelPassword.Location = new System.Drawing.Point(30, 110);
             this.labelPassword.Name = "labelPassword";
-            this.labelPassword.Size = new System.Drawing.Size(38, 15);
+            this.labelPassword.Size = new System.Drawing.Size(50, 22);
             this.labelPassword.TabIndex = 2;
             this.labelPassword.Text = "MSSV:";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(24, 100);
+            this.txtPassword.Location = new System.Drawing.Point(30, 140);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '●';
-            this.txtPassword.Size = new System.Drawing.Size(300, 23);
+            this.txtPassword.Size = new System.Drawing.Size(520, 30);
             this.txtPassword.TabIndex = 3;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(249, 140);
+            this.btnLogin.Location = new System.Drawing.Point(475, 190);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(75, 27);
+            this.btnLogin.Size = new System.Drawing.Size(75, 35);
             this.btnLogin.TabIndex = 4;
             this.btnLogin.Text = "Đăng nhập";
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             // 
             // Form1
             // 
             this.AcceptButton = this.btnLogin;
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(354, 190);
+            this.ClientSize = new System.Drawing.Size(584, 261);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.labelPassword);
@@ -100,6 +103,8 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Đăng nhập";
+            // set a larger default font for the form
+            this.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ResumeLayout(false);
             this.PerformLayout();
         }
